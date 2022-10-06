9705413cda3fdbf509056e7cea391a0c93754bbf
GonzaAv <gonzaloavila2727@gmail.com>
2022-10-06 11:36:48 -0300

    desafio2 async

--- desafio2/desafio2.js
+++ desafio2/desafio2.js
@@ -3,62 +3,76 @@ const fs = require("fs");
 const productList = "products.json"; //NO me tira bien la direccion :/
 
 
-const readArchive = () => {
-    const data = fs.readFileSync(productList, 'utf-8');
-    return JSON.parse(data);
-}
-
-const saveProduct = (products) => {
-    const data = JSON.stringify(products, null, '/t');
-    fs.writeFileSync(productList, data);
-
-}
-
-const getAll = () => {
-    const allProducts = readArchive();
-    return allProducts;
-}
-
-const getById = (searchId) => {
-    const products = readArchive();
-
-    const index = products.findIndex((aProduct) => aProduct.id === searchId)
-    if(index < 0) {
-        throw new Error("product don't exist")
-    }
-    return arrayMap(index);
-}
-
-const save = (data) => {
-    if (!data.title || !data.price || typeof data.title !== 'string' || typeof data.price !== 'number') throw new Error('Invalid Data');
-
-    const products = readArchive();
-
-    const newProduct = {
-        id: products[products.length -1].id + 1,
-        title: data.title,
-        price: data.price,
+class Container {
+    constructor(productList) {
+        if(!fs.existsSync(productList)) {
+            const dataInit = [];
+            saveProduct(dataInit);
+        }
+        fs.writeFileSync(productList, JSON.stringify([]));
     }
 
-    products.push(newProduct);
-    saveProduct(products);
+
+        readArchive = () => {
+            const data = fs.readFileSync(productList, 'utf-8');
+            return JSON.parse(data);
+        }
+
+        saveProduct = async (products) => {
+            const data = JSON.stringify(products, null, '/t');
+        await  fs.writeFileSync(productList, data);
+
+        }
+
+        getAll = async () => {
+            const allProducts = await readArchive();
+            return allProducts;
+        }
+
+        getById =  async (searchId) => {
+            const products = await readArchive();
+
+            const index = products.findIndex((aProduct) => aProduct.id === searchId)
+            if(index < 0) {
+                throw new Error("product don't exist")
+            }
+            return arrayMap(index);
+        }
+
+        save = async (data) => {
+            if (!data.title || !data.price || typeof data.title !== 'string' || typeof data.price !== 'number') throw new Error('Invalid Data');
+
+            const products = await readArchive();
+
+            const newProduct = {
+                id: products.length === 0 ? 1 : products[products.length -1].id + 1,
+                title: data.title,
+                price: data.price,
+            }
+
+            products.push(newProduct);
+            await saveProduct(products);
+        }
+
+        DelAll = async () => {
+        await saveProduct([]);
+        }
+
+        DelById = async (searchId) => {
+            const products = await readArchive();
+            const index = products.findIndex((aProduct) => aProduct.id === searchId);
+            if(index < 0) {
+                return;
+            }
+
+            products.splice(index, 1);
+            await saveProduct(products);
 }
 
-const DelAll = () => {
-    saveProduct([]);
 }
 
-const DelById = (searchId) => {
-    const products = readArchive();
-    const index = products.findIndex((aProduct) => aProduct.id === searchId);
-    if(index < 0) {
-        return;
-    }
 
-    products.splice(index, 1);
-    saveProduct(products);
 
-}
 save (
     {
         title: "CASA",
